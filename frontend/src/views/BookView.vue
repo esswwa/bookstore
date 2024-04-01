f8600d6221f96f31c9fca079b3ae867cae6fc736
qwerty <zoom.light@yandex.ru>
2024-04-01 15:05:34 +0300

    изменение фронта на более красивые выводы

--- frontend/src/views/BookView.vue
+++ frontend/src/views/BookView.vue
@@ -1,31 +1,37 @@
 <template>
-  <div class="max-w-sm w-full lg:max-w-full lg:flex">
-                    <div class="h-48 lg:h-auto lg:w-24 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" title="Woman holding a mug">
-                      </div>
-                          <div class="border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r m-2 p-8 flex flex-col justify-between leading-normal">
-                              <div class="mb-8">
-                                <p class="text-sm text-gray-600 flex items-center">
+<div class="max-w-sm rounded overflow-hidden shadow-lg ">
+
+      <div class="px-6 py-4">
+         <p class="text-sm text-gray-600 flex items-center">
                                   <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
                                   </svg>
-                                  Жанр
+                                  {{book.additional_genre.text_genre.text}}, {{book.author.text}}
                                 </p>
-                                <div class="text-gray-900 font-bold text-xl mb-2">{{book.name}}</div>
-                                <p class="text-gray-700 text-base">{{book.description}}</p>
-                              </div>
-                              <div class="flex items-center">
-                                <div class="text-sm">
-                                  <p class="text-gray-900 leading-none">{{ book.author }}</p>
-                                  <p class="text-gray-900 leading-none">{{ book.cost_per_one }}₽</p>
-                                  <p class="text-gray-600">{{book.rating}}</p>
-                                      <div class="px-6 py-4" >
-                                          <button v-if="favourites.includes(book.id)" @click="deleteFavourite(book.id)" class="py-4 px-6 m-2 bg-blue-400 text-white rounded-lg">Удалить из избранных</button>
-                                          <button v-else  @click="addBookToFavourite(book.id)" class="py-4 px-6 bg-blue-400 m-2 text-white rounded-lg">Добавить в избранное</button>
-                                      </div>
-                                </div>
-                            </div>
-                        </div>
+        <div class="font-bold text-xl mb-2">{{ book.name }}</div>
+        <p class="text-gray-700 text-base">{{ book.description }}</p>
       </div>
+      <div class="px-6 py-4">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Цена: {{ book.cost_per_one }}₽</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 ml-2">Рейтинг: {{book.rating}}</span>
+      </div>
+
+      <div class="px-6 py-4" >
+        <button @click="goToBook(book.id)" class="py-4 px-6 mr-2 bg-blue-400 text-white rounded-lg">Перейти</button>
+                                           <button v-if="favourites.includes(book.id)" @click="deleteFavourite(book.id)" class="py-4 px-6 text-black rounded-lg border border-blue-400">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="#60a5fa" stroke="isCurrent" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                                            </svg>
+                                          </button>
+
+                                        <button v-else @click="addBookToFavourite(book.id)" class="py-4 px-6 text-white rounded-lg border border-blue-400">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#60a5fa" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                                            </svg>
+                                          </button>
+      </div>
+
+    </div>
 </template>
 
 <script>
