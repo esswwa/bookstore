e9854e3c13698c48bf92c9e7ddc3edb3fe5e3805
esswwa <120464427+esswwa@users.noreply.github.com>
2024-05-12 00:15:26 +0500

    изменил аватарки

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -3,7 +3,9 @@
         <div class="main-center">
             <div class="p-4 bg-white border border-gray-200 rounded-lg flex flex-col items-center">
               <div class="flex items-center">
-                  <img src="https://i.pravatar.cc/40?img=70" class="w-16 rounded-full">
+                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 rounded-full">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
                   <p class="p-4" v-if="user.name"><strong>{{ user.name }}</strong></p>
               </div>
               <p class="p-4" v-if="user.email">{{ user.email }}</p>
