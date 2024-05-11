8fb0f8e4901733b3693d26dfdbc8b394469a357a
esswwa <120464427+esswwa@users.noreply.github.com>
2024-05-11 18:59:28 +0500

    изменил корзину, добавил проверку на количество на складе

--- frontend/src/views/BasketView.vue
+++ frontend/src/views/BasketView.vue
@@ -28,27 +28,33 @@
   <div class="flex flex-col">
     <div v-for="book in books" :key="book.id" v-if="books.length > 0">
       <div class="card rounded mt-4 p-4 overflow-hidden shadow-lg" :class="{'bg-red-200': book.count_on_stock === 0, 'bg-white': book.count_on_stock > 0 }">
-        <div @click="goToBook(book.id)" class="hover:bg-gray-100 duration-200 cursor-pointer rounded-lg card-content">
-                                <div class="px-6 py-4">
-                                  <p class="text-sm text-gray-600 flex items-center" v-if="book.author">
-                                  <svg v-if="!viewedBooks.includes(book.id)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-                                  </svg>
-                                  <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                  </svg>
-                                   {{ book.author.text.split(',')[0] }}
-                                      </p>
-                                      <div class="font-bold text-xl mb-2" v-if="book.name">{{book.name.slice(0,50) + (book.name.length > 50 ? '...' : '')}}</div>
-                                      <p class="text-gray-700 text-base" v-if="book.description">{{ book.description.slice(0, 200) + (book.name.length > 200 ? '...' : '') }}</p>
+        <div @click="goToBook(book.id)" class="flex hover:bg-gray-100 duration-200 cursor-pointer rounded-lg card-content">
+                           <div>
+                             <img class="p-2" style="height: 200px; width: 200px;" src="@/assets/preview.jpg" alt="Book cover">
+                           </div>
+                            <div class="px-6 py-4 flex flex-col">
+                              <div>
+                                 <p class="text-sm text-gray-600 flex items-center" v-if="book.author">
+                                <svg v-if="!viewedBooks.includes(book.id)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                                </svg>
+                                <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                </svg>
+                                {{ book.author.text.split(',')[0] }}
+                                </p>
+                                <div class="font-bold text-xl mb-2" v-if="book.name">{{book.name.slice(0,50) + (book.name.length > 50 ? '...' : '')}}</div>
+                                  <p class="text-gray-700 text-base" v-if="book.description">{{ book.description.slice(0, 200) + (book.name.length > 200 ? '...' : '') }}</p>
+                                </div>
+                              </div>
 
-                                    </div>
-                                    <div class="px-6 py-4 flex items-center">
-                                      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700" v-if="book.cost_per_one">Цена: {{ book.cost_per_one }} ₽</span>
-                                      <p class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold  ml-4">На складе {{ book.count_on_stock}} штук</p>
-                                    </div>
-        </div>
+                                <div class="px-6 py-4">
+                                  <span class="px-2 py-1 text-2xl font-semibold text-gray-700" v-if="book.cost_per_one">{{ book.cost_per_one }}₽</span>
+
+                                </div>
+                            </div>
+        <p class=" rounded-full px-6 py-4 text-sm font-semibold">На складе {{ book.count_on_stock}} штук(и)</p>
   <div class="mx-auto flex items-center px-3 text-sm">
                                       <div class="flex items-center m-2" v-if="book.rating >= 0">
                                           <svg class="w-4 h-4 text-yellow-300 me-1" v-if="book.rating > 0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
